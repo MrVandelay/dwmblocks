809996712f2903750560560edc259c7abc2ab476
fmober <moberg.fredrik@gmail.com>
2024-02-05 15:36:22 +0100

    ggg

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"\x3󰥻 \x1", "setxkbmap -query | grep layout | awk '{ print $2 }' | awk -F',' '{print $1}'",	3,		0},
+	{"\x3󰲝 \x1", "ip route get 8.8.8.8 2>/dev/null | grep -Eo 'src [0-9.]+' | grep -Eo '[0-9.]+'",	30,		0},
+	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"\x3 \x1", "date '+%Y-%m-%d %H:%M'",					5,		0},
+
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
